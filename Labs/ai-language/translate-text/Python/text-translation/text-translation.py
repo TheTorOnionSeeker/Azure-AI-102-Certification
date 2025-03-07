265473952c084a44faeb17960ea8792a73486650
TheTorOnionSeeker <guillermooscarn@gmail.com>
2025-03-06 21:49:18 -0300

    feat(Labs/):Added files for dir "ai-language/" => "language & speech & translate-text"

---
+++ Labs/ai-language/translate-text/Python/text-translation/text-translation.py
@@ -0,0 +1,107 @@
+from dotenv import load_dotenv
+import os
+import requests, json
+
+# import namespaces
+from azure.ai.translation.text import *
+from azure.ai.translation.text.models import InputTextItem
+
+""" def main():
+    global translator_endpoint
+    global cog_key
+    global cog_region
+
+    try:
+        # Get Configuration Settings
+        load_dotenv()
+        cog_key = os.getenv('COG_SERVICE_KEY')
+        cog_region = os.getenv('COG_SERVICE_REGION')
+        translator_endpoint = 'https://api.cognitive.microsofttranslator.com'
+
+        # Analyze each text file in the reviews folder
+        reviews_folder = 'reviews'
+        for file_name in os.listdir(reviews_folder):
+            # Read the file contents
+            print('\n-------------\n' + file_name)
+            text = open(os.path.join(reviews_folder, file_name), encoding='utf8').read()
+            print('\n' + text)
+
+            # Detect the language
+            language = GetLanguage(text)
+            print('Language:',language)
+
+            # Translate if not already English
+            if language != 'en':
+                translation = Translate(text, language)
+                print("\nTranslation:\n{}".format(translation))
+                
+    except Exception as ex:
+        print(ex)
+
+def GetLanguage(text):
+    # Default language is English
+    language = 'en'
+
+    # Use the Azure AI Translator detect function
+
+
+    # Return the language
+    return language
+
+def Translate(text, source_language):
+    translation = ''
+
+    # Use the Azure AI Translator translate function
+
+
+    # Return the translation
+    return translation """
+
+def main():
+    global translator_endpoint
+    global translatorKey
+    global translatorRegion
+
+    try:
+        # Get Configuration Settings
+        load_dotenv()
+        translatorKey = os.getenv('COG_SERVICE_KEY')
+        translatorRegion = os.getenv('COG_SERVICE_REGION')
+        translator_endpoint = 'https://api.cognitive.microsofttranslator.com'
+
+        # Create client using endpoint and key
+        credential = TranslatorCredential(translatorKey, translatorRegion)
+        client = TextTranslationClient(credential)
+
+        # Choose target language
+        languagesResponse = client.get_languages(scope="translation")
+        print("{} languages supported.".format(len(languagesResponse.translation)))
+        print("(See https://learn.microsoft.com/azure/ai-services/translator/language-support#translation)")
+        print("Enter a target language code for translation (for example, 'en'):")
+        targetLanguage = "xx"
+        supportedLanguage = False
+        while supportedLanguage == False:
+            targetLanguage = input()
+            if  targetLanguage in languagesResponse.translation.keys():
+                supportedLanguage = True
+            else:
+                print("{} is not a supported language.".format(targetLanguage))
+        
+        # Translate text
+        inputText = ""
+        while inputText.lower() != "quit":
+            inputText = input("Enter text to translate ('quit' to exit):")
+            if inputText != "quit":
+                input_text_elements = [InputTextItem(text=inputText)]
+                translationResponse = client.translate(content=input_text_elements, to=[targetLanguage])
+                translation = translationResponse[0] if translationResponse else None
+                if translation:
+                    sourceLanguage = translation.detected_language
+                    for translated_text in translation.translations:
+                        print(f"'{inputText}' was translated from {sourceLanguage.language} to {translated_text.to} as '{translated_text.text}'.")
+
+    except Exception as ex:
+        print(ex)
+
+if __name__ == "__main__":
+    main()
